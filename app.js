a2f01fe3ef7d1fcc7273056661b550a642c7ac93
VY <veasna.youophon@live.com>
2023-10-13 12:44:21 -0700

    Update home page text

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const Post = mongoose.model("Post", postSchema);
 
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "All blogs will be posted here at the home main screen. Enjoy posting your blogs!";
 const aboutContent = "We Da Best! 😜";
 const contactContent = "The Big-V Enterprise don't reply back to loser, talk among yourself Playa!!  😁";
 
